c7eaab545f3e927442a72aa1a1e3137a855667cf
ermias alehegn <ermiaseat1987@gmail.com>
2024-09-26 16:25:07 +0300

    updating service section and starting projects section

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,16 @@
-import Services from "../components/services/services";
 import Hero from "../components/hero/Hero";
 import ContactSection from "../components/contact-section/contact";
 import AboutMe from "../components/about-me/about";
-
-
+import ServiceSection from "../components/services/service-section";
+import ProjectSection from "../components/projects/project-section";
 
 export default async function Home() {
- 
-  
   return (
     <main className="flex flex-col min-h-screen gap-[3rem]  ">
       <Hero />
       <AboutMe />
-      <Services  />
+      <ServiceSection />
+      <ProjectSection />
       <ContactSection />
     </main>
   );
--- components/projects/project-section.tsx
+++ components/projects/project-section.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const ProjectSection = () => {
   return (
-    <section id='projects' className=''>ProjectSection</section>
-  )
-}
+    <section
+      id="projects"
+      className="flex flex-col items-center justify-center dark:text-white px-[5%] tablet:px-[10%] desktop:px-[17%]"
+    >
+      <h1 className="tablet:text-6xl mb-0">My Projects</h1>
+      <p className=" tablet:px-[100px]">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati ipsam
+        a tempore sapiente, nobis labore atque, corrupti rerum itaque unde neque
+        fugit eos similique quasi odio quo laudantium, minus sunt.
+      </p>
+      <div className="flex flex-col gap-3 tablet:flex-row"></div>
+    </section>
+  );
+};
 
-export default ProjectSection
+export default ProjectSection;
